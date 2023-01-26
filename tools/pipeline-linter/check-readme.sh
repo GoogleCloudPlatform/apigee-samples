58fe555063f1104a8aea5396857f596dac501495
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2023-01-26 00:29:35 -0800

    fix: readme issue

--- tools/pipeline-linter/check-readme.sh
+++ tools/pipeline-linter/check-readme.sh
@@ -24,11 +24,10 @@ for file in "$PWD"/*; do
     F=$(basename -- "$file")
     case $F in
         *.txt|*.md|tools) continue;;   ##: Skip files that matched.
-        *) grep "^-" README.md | grep "$F" -q || ERRORS="$ERRORS\n[ERROR] missing root README entry for $F";;                 ##: Change permission to the rest.
-    esac                
+        *) grep "^-" README.md | grep "$F" -q || ERRORS="$ERRORS\n[ERROR] missing root README entry for $F";;
 done
 
 if [ -n "$ERRORS" ]; then
-  echo "$ERRORS"
-  exit 1
+    echo "$ERRORS"
+    exit 1
 fi
