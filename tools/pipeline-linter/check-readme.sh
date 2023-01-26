e07bc409e3363f32a7abe36c9e74ef28f0bf5614
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2023-01-25 18:34:38 -0800

    fix: sh issue

--- tools/pipeline-linter/check-readme.sh
+++ tools/pipeline-linter/check-readme.sh
@@ -23,11 +23,11 @@ ERRORS=""
 for TYPE in $PWD; do
   for D in "$TYPE"/*; do
     F="$(basename $D)"
-    if [[ ! $F =~ ("tools"|".md"|".txt"|".sh")$ ]]
-    then 
-      echo $F
-      #grep "^-" README.md | grep $F -q || ERRORS="$ERRORS\n[ERROR] missing root README entry for $F"
-    fi
+    echo $F
+    # if [[ ! $F =~ ("tools"|".md"|".txt"|".sh")$ ]]
+    # then
+    #   #grep "^-" README.md | grep $F -q || ERRORS="$ERRORS\n[ERROR] missing root README entry for $F"
+    # fi
   done
 done
 
