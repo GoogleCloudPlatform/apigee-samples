7ede3b2388a877ee94c3d892bbc8ab0f5b19f28c
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2023-01-27 17:47:28 -0800

    fix: lint issues

--- tools/pipeline-linter/check-apigeelint.sh
+++ tools/pipeline-linter/check-apigeelint.sh
@@ -21,11 +21,13 @@ set -e
 
 # For API Proxies
 for proxyDir in "$PWD"/*/apiproxy; do
+    echo "Running apigeelint on $proxyDir"
     apigeelint -s "$proxyDir" -f table.js -e PO013,PO025 -x tools/pipeline-linter/apigeelint
 done
 
 # For Sharedflows
 for sfDir in "$PWD"/*/sharedflowbundle; do
+    echo "Running apigeelint on $sfDir"
     apigeelint -s "$sfDir" -f table.js -e PO013,PO025
 done
 
