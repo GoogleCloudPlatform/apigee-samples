5a80ea39c6a51cb551f2ca4b5a0047a09a5c5136
Ruben Gonzalez <gonzalezruben@google.com>
2023-02-09 21:33:25 -0600

    fix(deploy): not operator linting

--- authorize-idp-access-tokens/deploy-authorize-idp-access-tokens.sh
+++ authorize-idp-access-tokens/deploy-authorize-idp-access-tokens.sh
@@ -105,7 +105,7 @@ echo "Deploying Apigee artifacts..."
 mkdir rendered
 cp -r ./sharedflowbundle ./rendered
 sed -i "s/REPLACEWITHIDPCLIENTIDCLAIM/$TOKEN_CLIENT_ID_CLAIM/g" ./rendered/sharedflowbundle/policies/VK-IdentifyClientApp.xml
-if [ -n -z "$PR_KEY" ]; then
+if ! [ -z "$PR_KEY" ]; then
     echo "Deploying public and private keys for mock oidc..."
     echo -e "jwk=$JWK\nprivate_key=$PR_KEY" > mock_configuration.properties
     apigeecli res create --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --name mock_configuration --type properties --respath mock_configuration.properties
@@ -130,7 +130,7 @@ echo "Creating Developer App"
 apigeecli apps create --name $APP_NAME --email authz-idp-acccess-tokens_apigeesamples@acme.com --prods authz-idp-acccess-tokens-sample-product --callback https://developers.google.com/oauthplayground/ --org "$PROJECT" --token "$TOKEN" --disable-check
 
 
-if [ -n -z "$PR_KEY" ]; then
+if ! [ -z "$PR_KEY" ]; then
     TOKEN_AUDIENCE=$(apigeecli apps get --name $APP_NAME --org "$PROJECT" --token "$TOKEN" --disable-check | jq ."[0].credentials[0].consumerKey" -r)
     IDP_APP_CLIENT_ID="$TOKEN_AUDIENCE"
     IDP_APP_CLIENT_SECRET=$(apigeecli apps get --name $APP_NAME --org "$PROJECT" --token "$TOKEN" --disable-check | jq ."[0].credentials[0].consumerSecret" -r)
@@ -151,7 +151,7 @@ apigeecli res create --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --nam
 
 echo " "
 echo "All the Apigee artifacts are successfully deployed!"
-if [ -n -z "$PR_KEY" ]; then
+if ! [ -z "$PR_KEY" ]; then
     AUTHZ_ENDPOINT="https://$APIGEE_HOST/v1/samples/oidc/authorize"
     TOKEN_ENDPOINT="https://$APIGEE_HOST/v1/samples/oidc/token"
     ENCODED_AUTHZ_ENDPOINT=$(printf '%s\n' "$AUTHZ_ENDPOINT" | jq -sRr @uri)
