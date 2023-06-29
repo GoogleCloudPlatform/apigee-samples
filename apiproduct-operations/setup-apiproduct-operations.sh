a785776d70e29b1c2da255859f4c915d33bbc0fe
Dino Chiesa <dchiesa@google.com>
2023-06-28 18:20:59 -0700

    WIP add new apiproduct-operations sample
    This sample is mostly complete. Scripts are working,
    cloudshell tutorial is working.
    
    Remaining to do:
      - there's no video recording.
    
      - in the README, the URL for the video recording is wrong.
        It does not exist yet.

---
+++ apiproduct-operations/setup-apiproduct-operations.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PROXY_NAME=apiproduct-operations
+OAUTH_CC_PROXY_NAME=apiproduct-operations-oauth2
+
+create_apiproduct() {
+    local product_name=$1
+    local ops_file="./configuration-data/operations-defn-${product_name}.json"
+    apigeecli products get --name "${product_name}" --org "$PROJECT" --token "$TOKEN" --disable-check >> /dev/null 2>&1
+    if [[ $? -ne 0 ]] ; then
+        [[ ! -f "$ops_file" ]] && printf "missing operations definition file %s\n" "$ops_file" && exit 1
+        apigeecli products create --name "${product_name}" --displayname "${product_name}" \
+          --opgrp $ops_file \
+          --envs "$APIGEE_ENV" --approval auto \
+          --org "$PROJECT" --token "$TOKEN"
+    else
+        printf "  The apiproduct %s already exists!\n" ${product_name}
+    fi
+}
+
+create_app() {
+    local product_name=$1
+    local developer=$2
+    local app_name=${product_name}-app
+    local KEYPAIR
+
+    local NUM_APPS=$(apigeecli apps get --name ${app_name} --org "$PROJECT" --token "$TOKEN" --disable-check | jq -r .'| length')
+    if [[ $NUM_APPS -eq 0 ]] ; then
+        IFS=$' ' KEYPAIR=( $(apigeecli apps create --name ${app_name} --email ${developer} --prods ${product_name} --org "$PROJECT" --token "$TOKEN" --disable-check | jq -r ".credentials[0] | .consumerKey,.consumerSecret" ) )
+    else
+        # must not echo here, it corrupts the return value of the function.
+        # printf "  The app %s already exists!\n" ${app_name}
+        IFS=$' ' KEYPAIR=( $(apigeecli apps get --name ${app_name} --org "$PROJECT" --token "$TOKEN" --disable-check | jq -r ".[0].credentials[0] | .consumerKey,.consumerSecret" ) )
+    fi
+    echo ${KEYPAIR}
+}
+
+import_and_deploy_apiproxy() {
+    local proxy_name=$1
+    REV=$(apigeecli apis create bundle -f ./bundles/${proxy_name}/apiproxy  -n $proxy_name --org "$PROJECT" --token "$TOKEN" --disable-check | jq ."revision" -r)
+    apigeecli apis deploy --wait --name $proxy_name --ovr --rev "$REV" --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --disable-check
+}
+
+[[ -z "$PROJECT" ]] && echo "No PROJECT variable set" && exit 1
+[[ -z "$APIGEE_ENV" ]] && echo "No APIGEE_ENV variable set" && exit 1
+[[ -z "$APIGEE_HOST" ]] && echo "No APIGEE_HOST variable set" && exit 1
+
+TOKEN=$(gcloud auth print-access-token)
+
+echo "Installing dependencies"
+npm install
+
+echo "Installing apigeecli"
+curl -s https://raw.githubusercontent.com/apigee/apigeecli/master/downloadLatest.sh | bash
+export PATH=$PATH:$HOME/.apigeecli/bin
+
+echo "Running apigeelint"
+node_modules/apigeelint/cli.js -s ./bundles/apiproduct-operations-oauth2/apiproxy -f table.js
+node_modules/apigeelint/cli.js -s ./bundles/apiproduct-operations/apiproxy -f table.js
+
+echo "Configuring Apigee artifacts..."
+
+echo "Importing and Deploying Apigee the proxies..."
+import_and_deploy_apiproxy $PROXY_NAME
+import_and_deploy_apiproxy $OAUTH_CC_PROXY_NAME
+
+echo "Creating API Products"
+create_apiproduct "apiproduct-operations-viewer"
+create_apiproduct "apiproduct-operations-creator"
+create_apiproduct "apiproduct-operations-admin"
+
+DEVELOPER_EMAIL="${PROXY_NAME}-apigeesamples@acme.com"
+printf "Creating Developer %s\n" ${DEVELOPER_EMAIL}
+apigeecli developers get --email ${DEVELOPER_EMAIL} --org "$PROJECT" --token "$TOKEN" --disable-check >> /dev/null 2>&1
+if [[ $? -ne 0 ]] ; then
+    apigeecli developers create --user "${DEVELOPER_EMAIL}" --email "${DEVELOPER_EMAIL}" \
+          --first APIProduct --last SampleDeveloper \
+          --org "$PROJECT" --token "$TOKEN"
+else
+    printf "  The developer already exists.\n"
+fi
+
+echo "Checking and possibly Creating Developer Apps"
+VIEWER_CLIENT_CREDS=( $(create_app "apiproduct-operations-viewer" ${DEVELOPER_EMAIL}) )
+CREATOR_CLIENT_CREDS=( $(create_app "apiproduct-operations-creator" ${DEVELOPER_EMAIL}) )
+ADMIN_CLIENT_CREDS=( $(create_app "apiproduct-operations-admin" ${DEVELOPER_EMAIL}) )
+
+
+# these vars are all expected by integration tests (apickli)
+export SAMPLE_PROXY_BASEPATH="/v1/samples/$PROXY_NAME"
+export VIEWER_CLIENT_ID=${VIEWER_CLIENT_CREDS[0]}
+export CREATOR_CLIENT_ID=${CREATOR_CLIENT_CREDS[0]}
+export ADMIN_CLIENT_ID=${ADMIN_CLIENT_CREDS[0]}
+export VIEWER_CLIENT_SECRET=${VIEWER_CLIENT_CREDS[1]}
+export CREATOR_CLIENT_SECRET=${CREATOR_CLIENT_CREDS[1]}
+export ADMIN_CLIENT_SECRET=${ADMIN_CLIENT_CREDS[1]}
+
+# run the integration tests
+npm run test
+
+echo " "
+echo "All the Apigee artifacts are successfully deployed."
+echo " "
+echo "Copy/paste these statements into cloud shell to set variables for your proxy basepath"
+echo "for this sample, and for your API Keys and secrets:"
+echo "  export SAMPLE_PROXY_BASEPATH=$SAMPLE_PROXY_BASEPATH"
+echo "  export VIEWER_CLIENT_ID=$VIEWER_CLIENT_ID"
+echo "  export CREATOR_CLIENT_ID=$CREATOR_CLIENT_ID"
+echo "  export ADMIN_CLIENT_ID=$ADMIN_CLIENT_ID"
+echo "  export VIEWER_CLIENT_SECRET=$VIEWER_CLIENT_SECRET"
+echo "  export CREATOR_CLIENT_SECRET=$CREATOR_CLIENT_SECRET"
+echo "  export ADMIN_CLIENT_SECRET=$ADMIN_CLIENT_SECRET"
+echo " "
+echo "You need the CLIENT_SECRET variables only if you will be using the access token examples."
+echo " "
+echo "-----------------------------"
+echo " "
+echo "To call the API manually, copy/paste the above lines to set variables, and"
+echo "then run requests, like the following:"
+echo " "
+echo "curl -i -X GET https://${APIGEE_HOST}${SAMPLE_PROXY_BASEPATH}/apikey/users -H APIKEY:\$VIEWER_CLIENT_ID"
+echo "curl -i -X GET https://${APIGEE_HOST}${SAMPLE_PROXY_BASEPATH}/apikey/users -H APIKEY:\$CREATOR_CLIENT_ID"
+echo "curl -i -X POST https://${APIGEE_HOST}${SAMPLE_PROXY_BASEPATH}/apikey/users/1234 -d '' -H APIKEY:\$VIEWER_CLIENT_ID"
+echo "curl -i -X POST https://${APIGEE_HOST}${SAMPLE_PROXY_BASEPATH}/apikey/users/1234 -d '' -H APIKEY:\$CREATOR_CLIENT_ID"
+echo " "
---
+++ apiproduct-operations/test/integration/features/apiproduct-operations-token.feature
@@ -0,0 +1,123 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Feature:
+  As an Apigee platform explorer
+  I want to experiment with API Product Operations while using OAuth tokens
+  So that I can understand how Operations work
+
+  Scenario Outline: missing Token
+    When I GET /token/users
+    Then response code should be 401
+    And response body path $.fault.detail.errorcode should be oauth.v2.InvalidAccessToken
+
+  Scenario Outline: get a token for viewer
+    Given I have basic authentication credentials `viewerClientId` and `viewerClientSecret`
+      And I set Content-Type header to application/x-www-form-urlencoded
+      And I set body to grant_type=client_credentials
+    When I POST to -oauth2-cc/token
+    Then response code should be 200
+      And response body path $.token_type should be Bearer
+      And I store the value of body path $.access_token as VIEWER_ACCESSTOKEN in global scope
+
+  Scenario Outline: get a token for creator
+    Given I have basic authentication credentials `creatorClientId` and `creatorClientSecret`
+      And I set Content-Type header to application/x-www-form-urlencoded
+      And I set body to grant_type=client_credentials
+    When I POST to -oauth2-cc/token
+    Then response code should be 200
+      And response body path $.token_type should be Bearer
+      And I store the value of body path $.access_token as CREATOR_ACCESSTOKEN in global scope
+
+  Scenario Outline: get a token for admin
+    Given I have basic authentication credentials `adminClientId` and `adminClientSecret`
+      And I set Content-Type header to application/x-www-form-urlencoded
+      And I set body to grant_type=client_credentials
+    When I POST to -oauth2-cc/token
+    Then response code should be 200
+      And response body path $.token_type should be Bearer
+      And I store the value of body path $.access_token as ADMIN_ACCESSTOKEN in global scope
+
+  Scenario Outline: unknown resource
+    Given I set TOKEN header to `VIEWER_ACCESSTOKEN`
+    When I GET /token/unknown-resource
+    Then response code should be 400
+    And response body path $.response.message should be that request was unknown
+
+  Scenario Outline: list users ok
+    Given I set TOKEN header to `VIEWER_ACCESSTOKEN`
+    When I GET /token/users
+    Then response code should be 200
+      And response body path $.status should be OK
+      And response body path $.verb should be GET
+      And response body path $.apiproduct-operation-resource should be /*/users
+
+  Scenario Outline: list users reject
+    Given I set TOKEN header to `CREATOR_ACCESSTOKEN`
+    When I GET /token/users
+    Then response code should be 401
+    And response body path $.fault.detail.errorcode should be keymanagement.service.InvalidAPICallAsNoApiProductMatchFound
+
+  Scenario Outline: get single user ok
+    Given I set TOKEN header to `VIEWER_ACCESSTOKEN`
+    When I GET /token/users/1234
+    Then response code should be 200
+      And response body path $.status should be OK
+      And response body path $.verb should be GET
+      And response body path $.apiproduct-operation-resource should be /*/users/*
+
+  Scenario Outline: get single user reject
+    Given I set TOKEN header to `CREATOR_ACCESSTOKEN`
+    When I GET /token/users/1234
+    Then response code should be 401
+      And response body path $.fault.detail.errorcode should be keymanagement.service.InvalidAPICallAsNoApiProductMatchFound
+
+  Scenario Outline: create user reject
+    Given I set TOKEN header to `VIEWER_ACCESSTOKEN`
+      And I set Content-Type header to text/plain
+      And I set body to does-not-matter
+    When I POST to /token/users
+    Then response code should be 401
+      And response body path $.fault.detail.errorcode should be keymanagement.service.InvalidAPICallAsNoApiProductMatchFound
+
+  Scenario Outline: create user ok
+    Given I set TOKEN header to `CREATOR_ACCESSTOKEN`
+      And I set Content-Type header to text/plain
+      And I set body to does-not-matter
+    When I POST to /token/users
+    Then response code should be 200
+      And response body path $.status should be OK
+      And response body path $.verb should be POST
+      And response body path $.apiproduct-operation-resource should be /*/users
+
+
+  Scenario Outline: delete user reject 1
+    Given I set TOKEN header to `VIEWER_ACCESSTOKEN`
+    When I DELETE /token/users/1234
+    Then response code should be 401
+      And response body path $.fault.detail.errorcode should be keymanagement.service.InvalidAPICallAsNoApiProductMatchFound
+
+  Scenario Outline: delete user reject 2
+    Given I set TOKEN header to `CREATOR_ACCESSTOKEN`
+    When I DELETE /token/users/1234
+    Then response code should be 401
+      And response body path $.fault.detail.errorcode should be keymanagement.service.InvalidAPICallAsNoApiProductMatchFound
+
+  Scenario Outline: delete user ok
+    Given I set TOKEN header to `ADMIN_ACCESSTOKEN`
+    When I DELETE /token/users/1234
+    Then response code should be 200
+      And response body path $.status should be OK
+      And response body path $.verb should be DELETE
+      And response body path $.apiproduct-operation-resource should be /*/users/*
