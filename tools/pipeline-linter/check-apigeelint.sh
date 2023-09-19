b57e44333508fc478a08a7908317865d95d277dd
Eduardo Gonzalez <epbgonzalez@google.com>
2023-09-18 20:31:26 -0500

    fix: added bash prefix to megalinter

--- tools/pipeline-linter/check-apigeelint.sh
+++ tools/pipeline-linter/check-apigeelint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,7 +20,7 @@
 set -e
 
 exceptions=(
-"$PWD/grpc/apiproxy"
+    "$PWD/grpc/apiproxy"
 )
 # For API Proxies
 for proxyDir in "$PWD"/*/apiproxy; do
