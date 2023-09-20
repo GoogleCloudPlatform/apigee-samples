74f641c58f756e55df4decde5ac42d9e28a3c74e
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2023-09-20 08:03:11 -0700

    fix: apigeelinter

--- tools/pipeline-linter/check-apigeelint.sh
+++ tools/pipeline-linter/check-apigeelint.sh
@@ -19,16 +19,43 @@
 
 set -e
 
+
+#add any proxy that needs to be excluded. Needs review before adding any exclusions
+proxyExclusions=(
+'grpc'
+)
+
+#add any sharedflow that needs to be excluded. Needs review before adding any exclusions
+sfExclusions=(
+' '
+)
+
 # For API Proxies
-for proxyDir in "$PWD"/*/apiproxy; do
-  echo "Running apigeelint on $proxyDir"
-  apigeelint -s "$proxyDir" -f table.js -e PO013,PO025 -x tools/pipeline-linter/apigeelint --profile apigeex
+for proxyDir in "$PWD"/*/apiproxy "$PWD"/*/bundles/*/apiproxy; do
+    skip=false
+    for excl in "${proxyExclusions[@]}"; do
+        if [[ $proxyDir == *"$excl"* ]]; then
+            skip=true
+        fi
+    done
+    if [[ $skip = false ]]; then
+        echo "Running apigeelint on $proxyDir"
+        apigeelint -s "$proxyDir" -f table.js -e PO013,PO025 -x tools/pipeline-linter/apigeelint --profile apigeex
+    fi
 done
 
 # For Sharedflows
 for sfDir in "$PWD"/*/sharedflowbundle; do
-  echo "Running apigeelint on $sfDir"
-  apigeelint -s "$sfDir" -f table.js -e PO013,PO025 --profile apigeex
+    skip=false
+    for excl in "${sfExclusions[@]}"; do
+        if [[ $sfDir == *"$excl"* ]]; then
+            skip=true
+        fi
+    done
+    if [[ $skip = false ]]; then
+        echo "Running apigeelint on $sfDir"
+        apigeelint -s "$sfDir" -f table.js -e PO013,PO025 --profile apigeex
+    fi
 done
 
-echo
+echo 
