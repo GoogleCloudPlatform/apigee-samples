68951ea9686f5ce1dab159c59e8cf38ec4cf13ca
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2024-04-23 16:37:30 -0700

    chore: adding grpc-web

---
+++ grpc-web/README.md
@@ -0,0 +1,91 @@
+# gRPC-Web on Apigee
+
+This sample shows how to use Apigee in front of your gRPC backends using [gRPC-Web](https://github.com/grpc/grpc-web). In this example, we will show how Apigee can secure your grpc workload using the Threat Protection policies that are available.
+
+## Prerequisites
+
+1. [Provision Apigee X](https://cloud.google.com/apigee/docs/api-platform/get-started/provisioning-intro)
+2. Configure [external access](https://cloud.google.com/apigee/docs/api-platform/get-started/configure-routing#external-access) for API traffic to your Apigee X instance
+3. Access to deploy proxies, create products, apps and developers in Apigee
+4. Make sure the following tools are available in your terminal's $PATH (Cloud Shell has these preconfigured)
+    * [gcloud SDK](https://cloud.google.com/sdk/docs/install)
+    * unzip
+    * curl
+    * jq
+    * npm
+    * docker
+
+5. The following GCP APIs will be enabled:
+    * Cloud Run API
+    * Container Registry API
+    * Compute Engine API
+
+6. Make sure that you have ONE the following permissions:
+    * Owner role
+    * Editor role
+
+    The following set of roles:
+
+    * [Cloud Build Editor](https://cloud.google.com/build/docs/iam-roles-permissions) role
+    * [Artifact Registry Admin](https://cloud.google.com/iam/docs/understanding-roles#artifactregistry.repoAdmin) role
+    * [Storage Admin](https://cloud.google.com/storage/docs/access-control/iam-roles) role
+    * [Cloud Run Admin](https://cloud.google.com/build/docs/deploying-builds/deploy-cloud-run#fully-managed) role
+    * Service Account User role
+
+## (QuickStart) Setup using CloudShell
+
+Use the following GCP CloudShell tutorial, and follow the instructions.
+
+[![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.png)](https://ssh.cloud.google.com/cloudshell/open?cloudshell_git_repo=https://github.com/GoogleCloudPlatform/apigee-samples&cloudshell_git_branch=main&cloudshell_workspace=.&cloudshell_tutorial=grpc/docs/cloudshell-tutorial.md)
+
+## Setup instructions
+
+1. Clone the `apigee-samples` repo, and switch the `grpc` directory
+
+```bash
+git clone https://github.com/GoogleCloudPlatform/apigee-samples.git
+cd apigee-samples/grpc
+```
+
+2. Edit the `env.sh` file and configure the following variables:
+
+* `PROJECT` the project where your Apigee organization is located
+* `NETWORK` the VPC network where the PSC NEG will be deployed
+* `SUBNET` the VPC subnet where the PSC NEG will be deployed
+
+Now source the `env.sh` file
+
+```bash
+source ./env.sh
+```
+
+Let's run the script that will create and deploy the resources necessary to test the gRPC functionality. This script will create the following:
+
+* An External Loadbalancer with an HTTP2 backend
+* Deploy a sample gRPC Greeter service to Cloud Run
+* Deploy an API proxy, target server, developer, app and api product
+
+ Finally it tests that the deployment and configuration has been successful.
+
+3. Run the deploy.sh script:
+
+```sh
+./deploy.sh
+```
+
+## Manually Testing the gRPC Proxy
+
+To manually test the proxy, make requests using grpcurl or another gRPC client:
+
+```sh
+grpcurl -H "x-apikey:$CLIENT_ID" -import-path $PWD/grpc-backend/examples/protos -proto helloworld.proto -d '{"name\":"Guest"}' <YOUR_APIGEE_GRPC_HOSTNAME>:443 helloworld.Greeter/SayHello"
+```
+
+## Cleanup
+
+If you want to clean up the artifacts from this example in your Apigee Organization, first source your `env.sh` script, and then run
+
+```bash
+./clean-up.sh
+```
+
---
+++ grpc-web/clean-up.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -z "$PROJECT" ]; then
+  echo "No PROJECT variable set"
+  exit
+fi
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo "jq command is not on your PATH"
+  exit
+fi
+
+function wait_for_operation() {
+  while true; do
+    STATE="$(apigeecli operations get -o "$PROJECT" -n "$1" -t "$TOKEN" | jq --raw-output '.metadata.state')"
+    if [ "$STATE" = "FINISHED" ]; then
+      echo
+      break
+    fi
+    echo -n .
+    sleep 5
+  done
+}
+
+echo "Installing apigeecli"
+curl -s https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.sh | bash
+export PATH=$PATH:$HOME/.apigeecli/bin
+
+TOKEN="$(gcloud auth print-access-token)"
+
+# Get Org and instance information
+INSTANCE_JSON=$(apigeecli instances list -o "$PROJECT" -t "$TOKEN")
+INSTANCE_NAME=$(echo "$INSTANCE_JSON" | jq --raw-output '.instances[0].name')
+REGION=$(echo "$INSTANCE_JSON" | jq --raw-output '.instances[0].location')
+ENVIRONMENT_NAME="sample-environment"
+ENVIRONMENT_GROUP_NAME="sample-environment-group"
+
+echo "Deleting load balancer..."
+# Delete forwarding rule
+gcloud compute forwarding-rules delete sample-apigee-https-lb-rule \
+  --global \
+  --project="$PROJECT" --quiet
+
+# Delete target HTTPS proxy
+gcloud compute target-https-proxies delete sample-apigee-https-proxy \
+  --project="$PROJECT" --quiet
+
+# Delete URL map
+gcloud compute url-maps delete sample-apigee-urlmap \
+  --project="$PROJECT" --quiet
+
+# Delete backend service
+gcloud compute backend-services delete sample-apigee-backend \
+  --global \
+  --project="$PROJECT" --quiet
+
+# Delete NEG
+gcloud compute network-endpoint-groups delete sample-apigee-neg \
+  --region="$REGION" \
+  --project="$PROJECT" --quiet
+
+# Delete cert
+echo "Deleting SSL certificate..."
+gcloud compute ssl-certificates delete sample-apigee-ssl-cert \
+  --project "$PROJECT" --quiet
+
+# Delete VIP
+echo "Deleting load balancer IP address..."
+gcloud compute addresses delete sample-apigee-vip \
+  --global \
+  --project "$PROJECT" --quiet
+
+#Undeploy gRPC service from Cloud Run and delete images
+echo "Deleting Cloud Run gRPC service..."
+gcloud run services delete grpc-backend-apigee --region "$REGION" --quiet
+
+echo "Deleting Developer Apps..."
+DEVELOPER_ID=$(apigeecli developers get --email grpc_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'developerId' -r)
+apigeecli apps delete --id "$DEVELOPER_ID" --name grpcApp --org "$PROJECT" --token "$TOKEN"
+
+echo "Deleting Developer..."
+apigeecli developers delete --email grpc_apigeesamples@acme.com --org "$PROJECT" --token "$TOKEN"
+
+echo "Deleting API Products..."
+apigeecli products delete --name grpc --org "$PROJECT" --token "$TOKEN"
+
+echo "Undeploying grpc proxy..."
+REV=$(apigeecli envs deployments get --env "$ENVIRONMENT_NAME" --org "$PROJECT" --token "$TOKEN" --disable-check | jq .'deployments[]| select(.apiProxy=="grpc").revision' -r)
+apigeecli apis undeploy --name grpc --env "$ENVIRONMENT_NAME" --rev "$REV" --org "$PROJECT" --token "$TOKEN"
+
+echo "Deleting grpc proxy..."
+apigeecli apis delete --name grpc --org "$PROJECT" --token "$TOKEN"
+
+echo "Deleting gRPC target server..."
+apigeecli targetservers delete --name grpc-server --org "$PROJECT" --token "$TOKEN" -e "$ENVIRONMENT_NAME"
+
+echo -n "Detaching environment from group..."
+OPERATION=$(apigeecli envgroups detach -o "$PROJECT" -e "$ENVIRONMENT_NAME" -n "$ENVIRONMENT_GROUP_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+wait_for_operation "$OPERATION"
+
+echo -n "Deleting environment group..."
+OPERATION=$(apigeecli envgroups delete -o "$PROJECT" -n "$ENVIRONMENT_GROUP_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+wait_for_operation "$OPERATION"
+
+echo -n "Detaching environment from instance..."
+OPERATION=$(apigeecli instances attachments detach -o "$PROJECT" -e "$ENVIRONMENT_NAME" -n "$INSTANCE_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+wait_for_operation "$OPERATION"
+
+echo -n "Deleting environment..."
+OPERATION=$(apigeecli environments delete -o "$PROJECT" -e "$ENVIRONMENT_NAME" -t "$TOKEN" | jq --raw-output '.name' | awk -F/ '{print $4}')
+wait_for_operation "$OPERATION"
+
+echo "Clean up complete!"
