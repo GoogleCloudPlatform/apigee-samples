ddbe1616b6515147899fe57b31684031d0488d83
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2025-05-07 18:29:48 -0700

    fix: linting issues

--- adk-auto-insurance-agent/cleanup-adk-auto-insurance-agent.sh
+++ adk-auto-insurance-agent/cleanup-adk-auto-insurance-agent.sh
@@ -42,7 +42,7 @@ delete_api() {
 
 delete_api_from_hub() {
   local api=$1
-  apigeecli apihub apis delete --id ${api}_api \
+  apigeecli apihub apis delete --id "${api}_api" \
   --force true \
   -r "$APIGEE_APIHUB_REGION" -o "$APIGEE_APIHUB_PROJECT_ID" -t "$TOKEN"
 }
