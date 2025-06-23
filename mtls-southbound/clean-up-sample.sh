f7cb66f93011dd88553b9af2b559b7eaadb70c87
tyayers <tyler.ayers@gmail.com>
2025-06-23 17:55:23 +0200

    fix(mtls-southbound): removed comments from cleanup script

--- mtls-southbound/clean-up-sample.sh
+++ mtls-southbound/clean-up-sample.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 gcloud compute instances delete $VM_NAME --zone $ZONE --project $PROJECT_ID
-# undeploy and delete Apigee proxy
 apigeecli apis undeploy -e $APIGEE_ENV -n mtls-southbound-v1 -o $PROJECT_ID -t $(gcloud auth print-access-token)
 apigeecli apis delete -n mtls-southbound-v1 -o $PROJECT_ID -t $(gcloud auth print-access-token)
-# delete Apigee Target and Keystore
 apigeecli targetservers delete -n mtls-service -e $APIGEE_ENV -o $PROJECT_ID -t $(gcloud auth print-access-token)
 apigeecli keystores delete -n mtls-keystore1 -o $PROJECT_ID -e $APIGEE_ENV -o $PROJECT_ID -t $(gcloud auth print-access-token)
