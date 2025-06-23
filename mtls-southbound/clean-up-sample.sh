4f06c4d94d931b41e5abe72d0933aa9477228e85
tyayers <tyler.ayers@gmail.com>
2025-06-23 21:52:17 +0200

    fix(mtls-southbound): fixed cleanup lint issue

--- mtls-southbound/clean-up-sample.sh
+++ mtls-southbound/clean-up-sample.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 gcloud compute instances delete "$VM_NAME" --zone "$ZONE" --project "$PROJECT_ID" --quiet
-apigeecli apis undeploy -e "$APIGEE_ENV" -n mtls-southbound-v1 -o "$PROJECT_ID" -t $(gcloud auth print-access-token)
-apigeecli apis delete -n mtls-southbound-v1 -o "$PROJECT_ID" -t $(gcloud auth print-access-token)
-apigeecli targetservers delete -n mtls-service -e "$APIGEE_ENV" -o "$PROJECT_ID" -t $(gcloud auth print-access-token)
-apigeecli keystores delete -n mtls-keystore1 -o "$PROJECT_ID" -e "$APIGEE_ENV" -o "$PROJECT_ID" -t $(gcloud auth print-access-token)
+apigeecli apis undeploy -e "$APIGEE_ENV" -n mtls-southbound-v1 -o "$PROJECT_ID" -t "$(gcloud auth print-access-token)"
+apigeecli apis delete -n mtls-southbound-v1 -o "$PROJECT_ID" -t "$(gcloud auth print-access-token)"
+apigeecli targetservers delete -n mtls-service -e "$APIGEE_ENV" -o "$PROJECT_ID" -t "$(gcloud auth print-access-token)"
+apigeecli keystores delete -n mtls-keystore1 -o "$PROJECT_ID" -e "$APIGEE_ENV" -o "$PROJECT_ID" -t "$(gcloud auth print-access-token)"
