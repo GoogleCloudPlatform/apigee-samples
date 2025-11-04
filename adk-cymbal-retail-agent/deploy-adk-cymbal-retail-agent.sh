35e0ebb0ef3c9a0bfb2537d1d434a8c6b30355df
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2025-11-03 20:31:41 -0800

    chore: Update go-gen version

--- adk-cymbal-retail-agent/deploy-adk-cymbal-retail-agent.sh
+++ adk-cymbal-retail-agent/deploy-adk-cymbal-retail-agent.sh
@@ -176,7 +176,7 @@ curl -s https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.s
 export PATH=$PATH:$HOME/.apigeecli/bin
 
 echo "✅ Installing apigee-go-gen tool ..."
-curl -s https://apigee.github.io/apigee-go-gen/install | bash -s v1.1.0-beta.1 ~/.apigee-go-gen/bin
+curl -s https://apigee.github.io/apigee-go-gen/install | bash -s v1.1.0-beta.9 ~/.apigee-go-gen/bin
 export PATH=$PATH:$HOME/.apigee-go-gen/bin
 
 echo "Installing dependencies..."
